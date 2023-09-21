d66a6dea4f4c0f9f1cb1516ad02ec3280ec4173b
Jekarter <jkartyalta@yandex.ru>
2023-09-21 18:07:41 +0300

    feat(filter): filled items

--- src/components/FormFilter/FormFilter.tsx
+++ src/components/FormFilter/FormFilter.tsx
@@ -17,22 +17,54 @@ const FormFilter = () => {
     <section className={styles.filter}>
       <h2 className={styles.title}>Фильтр упражнений</h2>
       <form onSubmit={onSubmit}>
-        <fieldset>
-          <legend>Мышцы по группам:</legend>
-          <ul>
-            <li>
-              <label>
-                <input type='checkbox' value=''></input>
-              </label>
+        <fieldset className={styles.group}>
+          <legend className={styles.titleFilter}>Где будет проходить тренировка:</legend>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <input className='visually-hidden' id='street' type='radio' name='place' value='street'></input>
+              <label htmlFor='street'>На улице/дома</label>
             </li>
-            <button type='submit'>Подтвердить</button>
-
+            <li className={styles.listItem}>
+              <input className='visually-hidden' id='gym' type='radio' name='place' value='gym'></input>
+              <label htmlFor='gym'>Тренажерный зал</label>
+            </li>
+            <li className={styles.listItem}>
+              <input className='visually-hidden' id='all' type='radio' name='place' value='all'></input>
+              <label htmlFor='all'>Посмотреть все</label>
+            </li>
           </ul>
         </fieldset>
-
+        <fieldset className={styles.group}>
+          <legend className={styles.titleFilter}>Мышцы по группам:</legend>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <input className='visually-hidden' id='pectoral' type='checkbox' value='pectoral'></input>
+              <label htmlFor='pectoral'>Грудные</label>
+            </li>
+            <li className={styles.listItem}>
+              <input className='visually-hidden' id='spinal' type='checkbox' value='spinal'></input>
+              <label htmlFor='spinal'>Спинные</label>
+            </li>
+            <li className={styles.listItem}>
+              <input className='visually-hidden' id='shoulder' type='checkbox' value='shoulder'></input>
+              <label htmlFor='shoulder'>Плечевые</label>
+            </li>
+            <li className={styles.listItem}>
+              <input className='visually-hidden' id='leg' type='checkbox' value='leg'></input>
+              <label htmlFor='leg'>Мышцы ног</label>
+            </li>
+            <li className={styles.listItem}>
+              <input className='visually-hidden' id='arm' type='checkbox' value='arm'></input>
+              <label htmlFor='arm'>Мышцы рук</label>
+            </li>
+            <li className={styles.listItem}>
+              <input className='visually-hidden' id='abdominal' type='checkbox' value='abdominal'></input>
+              <label htmlFor='abdominal'>Мышцы живота</label>
+            </li>
+          </ul>
+        </fieldset>
+        <button type='submit'>Подтвердить</button>
       </form>
-
-      
     </section>
   )
 }
