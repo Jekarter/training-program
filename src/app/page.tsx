d5c0eec5a298c6762769065a4645b684b0ca0afd
Jekarter <jkartyalta@yandex.ru>
2023-09-22 12:47:55 +0300

    feat(filter): add full filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
-import mainImage from '../../public/main-image.jpg'
+import mainImage from '../assets/main-image.jpg'
 import cl from 'classnames'
 import MainInfo from '@/components/MainInfo/MainInfo'
 import FormFilter from '@/components/FormFilter/FormFilter'
--- src/components/FormFilter/FormFilter.tsx
+++ src/components/FormFilter/FormFilter.tsx
@@ -16,21 +16,21 @@ const FormFilter = () => {
   return (
     <section className={styles.filter}>
       <h2 className={styles.title}>Фильтр упражнений</h2>
-      <form onSubmit={onSubmit}>
+      <form className={styles.filterForm} onSubmit={onSubmit}>
         <fieldset className={styles.group}>
           <legend className={styles.titleFilter}>Где будет проходить тренировка:</legend>
           <ul className={styles.list}>
             <li className={styles.listItem}>
               <input className='visually-hidden' id='street' type='radio' name='place' value='street'></input>
-              <label htmlFor='street'>На улице/дома</label>
+              <label className={styles.radioLabel} htmlFor='street'>На улице/дома</label>
             </li>
             <li className={styles.listItem}>
               <input className='visually-hidden' id='gym' type='radio' name='place' value='gym'></input>
-              <label htmlFor='gym'>Тренажерный зал</label>
+              <label className={styles.radioLabel} htmlFor='gym'>Тренажерный зал</label>
             </li>
             <li className={styles.listItem}>
               <input className='visually-hidden' id='all' type='radio' name='place' value='all'></input>
-              <label htmlFor='all'>Посмотреть все</label>
+              <label className={styles.radioLabel} htmlFor='all'>Посмотреть все</label>
             </li>
           </ul>
         </fieldset>
@@ -39,31 +39,34 @@ const FormFilter = () => {
           <ul className={styles.list}>
             <li className={styles.listItem}>
               <input className='visually-hidden' id='pectoral' type='checkbox' value='pectoral'></input>
-              <label htmlFor='pectoral'>Грудные</label>
+              <label className={styles.labelCheckbox} htmlFor='pectoral'>Грудные</label>
             </li>
             <li className={styles.listItem}>
               <input className='visually-hidden' id='spinal' type='checkbox' value='spinal'></input>
-              <label htmlFor='spinal'>Спинные</label>
+              <label className={styles.labelCheckbox} htmlFor='spinal'>Спинные</label>
             </li>
             <li className={styles.listItem}>
               <input className='visually-hidden' id='shoulder' type='checkbox' value='shoulder'></input>
-              <label htmlFor='shoulder'>Плечевые</label>
+              <label className={styles.labelCheckbox} htmlFor='shoulder'>Плечевые</label>
             </li>
             <li className={styles.listItem}>
               <input className='visually-hidden' id='leg' type='checkbox' value='leg'></input>
-              <label htmlFor='leg'>Мышцы ног</label>
+              <label className={styles.labelCheckbox} htmlFor='leg'>Мышцы ног</label>
             </li>
             <li className={styles.listItem}>
               <input className='visually-hidden' id='arm' type='checkbox' value='arm'></input>
-              <label htmlFor='arm'>Мышцы рук</label>
+              <label className={styles.labelCheckbox} htmlFor='arm'>Мышцы рук</label>
             </li>
             <li className={styles.listItem}>
               <input className='visually-hidden' id='abdominal' type='checkbox' value='abdominal'></input>
-              <label htmlFor='abdominal'>Мышцы живота</label>
+              <label className={styles.labelCheckbox} htmlFor='abdominal'>Мышцы живота</label>
             </li>
           </ul>
         </fieldset>
-        <button type='submit'>Подтвердить</button>
+        <div className={styles.buttonContainer}>
+          <button type='submit'>Подтвердить</button>
+          <button type='reset'>Сбросить фильтр</button>
+        </div>
       </form>
     </section>
   )
