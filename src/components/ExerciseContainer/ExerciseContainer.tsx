33c67239ce3845195c2474b251a5c966084d885b
Jekarter <jkartyalta@yandex.ru>
2023-09-29 17:25:45 +0300

    feat(exercise page): add back-button

--- src/components/ExerciseContainer/ExerciseContainer.tsx
+++ src/components/ExerciseContainer/ExerciseContainer.tsx
@@ -2,34 +2,50 @@ import { cardObject } from '@/types/types'
 import Image from 'next/image'
 import React from 'react'
 import styles from './exercisecontainer.module.scss'
+import Link from 'next/link'
 
 
 const ExerciseContainer = ({ ...props }: cardObject) => {
   return (
     <div>
-      <h1>{props.title}</h1>
       <div>
+        <Link href={'/'} >
+          <button className={styles.backButton}>Назад</button>
+        </Link>
+      </div>
+      <h1 className={styles.title}>{props.title}</h1>
+      <div className={styles.mainImageContainer}>
         <Image src={props.img} alt={props.title}  width={300} height={300} layout='responsive'/>
       </div>
-      <div>
+      <div className={styles.textContainer}>
         <h3>Исходное положение</h3>
-        <p>{props.description}</p>
+        <p className={styles.description}>{props.description}</p>
       </div>
-      <div>
+      <div className={styles.textContainer}>
         <h3>{props.subtitle}</h3>
-        <ol>
+        <ol className={styles.listActions}>
           {props.actionList?.map(item => (
             <li key={item}>{item}</li>
           ))}
         </ol>
       </div>
-      <div>
-        <Image src={props.imgSecond} alt={props.subtitle} width={300} height={300} />
-        <p>{props.descriptionSecond}</p>
+      <div className={styles.textContainer}>
+        <div className={styles.imageContainer}>
+          <Image src={props.imgSecond} alt={props.subtitle} width={300} height={300} />
+        </div>
+        <p className={styles.description}>{props.descriptionSecond}</p>
       </div>
-      <div>
+      <div className={styles.textContainer}>
+        <h3>Использование упражнения в бодибилдинге</h3>
+        <ul className={styles.listActions}>
+          {props.whoUseIt.map(use => (
+            <li key={use}>{use}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.textContainer}>
         <h3>Видео выполнения упражнения</h3>
-        <div dangerouslySetInnerHTML={{ __html: props.videoUrl}}></div>
+        <div className={styles.videoContainer} dangerouslySetInnerHTML={{ __html: props.videoUrl}}></div>
       </div>
     </div>
   )
